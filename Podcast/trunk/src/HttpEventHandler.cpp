3349c90c39f48f78da56843e28da4c1f933d1ee9
teknolog <teknolog@de3fedf9-773c-0410-aaf4-894f9644ab05>
2008-03-07 16:31:53 +0000

    Removed Sony Ericsson copyright left from sample code

--- Podcast/trunk/src/HttpEventHandler.cpp
+++ Podcast/trunk/src/HttpEventHandler.cpp
@@ -1,14 +1,3 @@
-/***************************************************************************** 
-COPYRIGHT All rights reserved Sony Ericsson Mobile Communications AB 2006. 
-The software is the copyrighted work of Sony Ericsson Mobile Communications AB. 
-The use of the software is subject to the terms of use or of the end-user license 
-agreement which accompanies or is included with the software. The software is 
-provided "as is" and Sony Ericsson specifically disclaim any warranty or 
-condition whatsoever regarding merchantability or fitness for a specific 
-purpose, title or non-infringement. No warranty of any kind is made in 
-relation to the condition, suitability, availability, accuracy, reliability, 
-merchantability and/or non-infringement of the software provided herein. 
-*****************************************************************************/
 
 // HttpEventHandler.cpp
 #include <e32debug.h>
