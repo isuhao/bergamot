823543c6a34e1e48007faa076ec20ad3327bb042
teknolog <teknolog@de3fedf9-773c-0410-aaf4-894f9644ab05>
2008-03-31 13:06:21 +0000

    Limited download progress bar to only show when the active download is selected, not sure this is optimal, but I'll try it out

--- Podcast/trunk/src/PodcastClientShowsView.cpp
+++ Podcast/trunk/src/PodcastClientShowsView.cpp
@@ -321,7 +321,21 @@ void CPodcastClientShowsView::ShowDownloadUpdatedL(TInt aPercentOfCurrentDownloa
 {
 	if(ViewContext() != NULL)
 	{
-		if(aPercentOfCurrentDownload>=0 && aPercentOfCurrentDownload < KOneHundredPercent)
+		CShowInfo *showInfo = NULL;
+		TInt index = iListbox->CurrentItemIndex();
+		if(index >= 0 && index < iPodcastModel.ActiveShowList().Count())
+		{
+					showInfo = iPodcastModel.ActiveShowList()[index];
+		}
+
+		// we show progress bar only for pending shows and inside the feed to which
+	    // the active download belongs
+		// (iCurrentCategory == EShowPendingShows ||
+		//iPodcastModel.ShowEngine().ShowDownloading()->FeedUid() == iPodcastModel.ActiveFeedInfo()->Uid()) &&
+
+		// now we only show when the active downloa has focus in a list
+		if(showInfo != NULL && showInfo->Uid() == iPodcastModel.ShowEngine().ShowDownloading()->Uid() &&				
+				aPercentOfCurrentDownload>=0 && aPercentOfCurrentDownload < KOneHundredPercent)
 		{
 			if(!iProgressAdded)
 			{
