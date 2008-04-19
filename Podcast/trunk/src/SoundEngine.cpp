1384641c1ba3159be3c15f4e5e9a5e49e1a30193
teknolog <teknolog@de3fedf9-773c-0410-aaf4-894f9644ab05>
2008-04-19 12:46:50 +0000

    Fixed TUint overflow bug when moving position > ~4200 seconds

--- Podcast/trunk/src/SoundEngine.cpp
+++ Podcast/trunk/src/SoundEngine.cpp
@@ -132,7 +132,7 @@ void CSoundEngine::SetPosition(TUint aPos)
 {
 	if(iState > ESoundEngineOpening)
 	{
-		TTimeIntervalMicroSeconds pos = aPos*1000000;
+		TTimeIntervalMicroSeconds pos = ((TUint64)aPos)*1000000;
 		if(iState == ESoundEnginePlaying)
 		{
 			iPlayer->Pause();
