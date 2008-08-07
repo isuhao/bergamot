7e7be8bde40a12093156987f97f5ddf642ed02a7
teknolog <teknolog@de3fedf9-773c-0410-aaf4-894f9644ab05>
2008-08-07 19:05:53 +0000

    Moving Engine from UIQ part to common

--- Podcast/branches/s60/uiq/inc/ShowEngineObserver.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHOWENGINEOBSERVER_H_
-#define SHOWENGINEOBSERVER_H_
-class MShowEngineObserver {
-public:
-	virtual void ShowListUpdated() = 0;
-	virtual void DownloadQueueUpdated(TInt aDownloadingShows, TInt aQueuedShows) = 0;
-	virtual void ShowDownloadUpdatedL(TInt aPercentOfCurrentDownload, TInt aBytesOfCurrentDownload, TInt aBytesTotal) = 0;
-};
-#endif /*SHOWENGINEOBSERVER_H_*/
