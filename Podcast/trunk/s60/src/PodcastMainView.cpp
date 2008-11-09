a39e70dc18f7fb38a9432bf249da50e2a0834819
LarsPson@gmail.com <LarsPson@gmail.com@de3fedf9-773c-0410-aaf4-894f9644ab05>
2008-11-09 20:55:22 +0000

    S60 formatting of code

--- Podcast/trunk/s60/src/PodcastMainView.cpp
+++ Podcast/trunk/s60/src/PodcastMainView.cpp
@@ -42,12 +42,10 @@ void CPodcastMainView::ConstructL()
 	CleanupStack::PushL( icons );
 
 	// Load the bitmap for play icon	
-	CFbsBitmap* bitmap = iEikonEnv->CreateBitmapL( _L("*"), 		
-													EMbmPodcastPlay_40x40);
+	CFbsBitmap* bitmap = iEikonEnv->CreateBitmapL( _L("*"),EMbmPodcastPlay_40x40);
 	CleanupStack::PushL( bitmap );		
 	// Load the mask for play icon	
-	CFbsBitmap* mask = iEikonEnv->CreateBitmapL( _L("*"), 		
-												EMbmPodcastPlay_40x40m );	
+	CFbsBitmap* mask = iEikonEnv->CreateBitmapL( _L("*"),EMbmPodcastPlay_40x40m );	
 	CleanupStack::PushL( mask );
 	// Append the play icon to icon array
 	icons->AppendL( CGulIcon::NewL( bitmap, mask ) );
@@ -55,12 +53,10 @@ void CPodcastMainView::ConstructL()
 
 
 	// Load the bitmap for shows icon	
-	bitmap = iEikonEnv->CreateBitmapL( _L("*"), 		
-													EMbmPodcastDownloaded_view_40x40);
+	bitmap = iEikonEnv->CreateBitmapL( _L("*"),EMbmPodcastDownloaded_view_40x40);
 	CleanupStack::PushL( bitmap );		
 	// Load the mask for shows icon	
-	mask = iEikonEnv->CreateBitmapL( _L("*"), 		
-												EMbmPodcastDownloaded_view_40x40m );	
+	mask = iEikonEnv->CreateBitmapL( _L("*"),EMbmPodcastDownloaded_view_40x40m );	
 	CleanupStack::PushL( mask );
 	// Append the shows icon to icon array
 	icons->AppendL( CGulIcon::NewL( bitmap, mask ) );
@@ -68,12 +64,10 @@ void CPodcastMainView::ConstructL()
 
 
 	// Load the bitmap for download icon	
-	bitmap = iEikonEnv->CreateBitmapL( _L("*"), 		
-													EMbmPodcastPending_view_40x40);
+	bitmap = iEikonEnv->CreateBitmapL( _L("*"),EMbmPodcastPending_view_40x40);
 	CleanupStack::PushL( bitmap );		
 	// Load the mask for download icon	
-	mask = iEikonEnv->CreateBitmapL( _L("*"), 		
-												EMbmPodcastPending_view_40x40m );	
+	mask = iEikonEnv->CreateBitmapL( _L("*"),EMbmPodcastPending_view_40x40m );	
 	CleanupStack::PushL( mask );
 	// Append the download icon to icon array
 	icons->AppendL( CGulIcon::NewL( bitmap, mask ) );
@@ -81,12 +75,10 @@ void CPodcastMainView::ConstructL()
 
 
 	// Load the bitmap for Feeds icon	
-	bitmap = iEikonEnv->CreateBitmapL( _L("*"), 		
-													EMbmPodcastFeeds_40x40);
+	bitmap = iEikonEnv->CreateBitmapL( _L("*"),EMbmPodcastFeeds_40x40);
 	CleanupStack::PushL( bitmap );		
 	// Load the mask for Feeds icon	
-	mask = iEikonEnv->CreateBitmapL( _L("*"), 		
-												EMbmPodcastFeeds_40x40m );	
+	mask = iEikonEnv->CreateBitmapL( _L("*"),EMbmPodcastFeeds_40x40m );	
 	CleanupStack::PushL( mask );
 	// Append the Feeds icon to icon array
 	icons->AppendL( CGulIcon::NewL( bitmap, mask ) );
@@ -94,19 +86,17 @@ void CPodcastMainView::ConstructL()
 
 
 	// Load the bitmap for audiobooks icon	
-	bitmap = iEikonEnv->CreateBitmapL( _L("*"), 		
-													EMbmPodcastAudiobooks_40x40);
+	bitmap = iEikonEnv->CreateBitmapL( _L("*"),EMbmPodcastAudiobooks_40x40);
 	CleanupStack::PushL( bitmap );		
 	// Load the mask for audiobooks icon	
-	mask = iEikonEnv->CreateBitmapL( _L("*"), 		
-												EMbmPodcastAudiobooks_40x40m );	
+	mask = iEikonEnv->CreateBitmapL( _L("*"),EMbmPodcastAudiobooks_40x40m );	
 	CleanupStack::PushL( mask );
 	// Append the audiobooks icon to icon array
 	icons->AppendL( CGulIcon::NewL( bitmap, mask ) );
 	CleanupStack::Pop(2); // bitmap, mask
 
 	iListContainer->Listbox()->ItemDrawer()->FormattedCellData()->SetIconArrayL( icons );
-	CleanupStack::Pop(); // icons
+	CleanupStack::Pop(icons); // icons
 
 	CDesCArray* items = iEikonEnv->ReadDesCArrayResourceL(R_PODCAST_MAINMENU_ARRAY);
 	iListContainer->Listbox()->Model()->SetItemTextArray(items);
