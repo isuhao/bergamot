2bd0e8712acdf69522ca6cd9b2e87c1fc454ce94
teknolog <teknolog@de3fedf9-773c-0410-aaf4-894f9644ab05>
2008-11-29 17:18:34 +0000

    Updated S60 pkg

---
+++ Podcast/trunk/s60/sis/make_s60_sis.cmd
@@ -0,0 +1,3 @@
+del *.sis
+makesis -v -dc:\symbian\s60_3rd_mr\ Podcast_udeb.pkg
+signsis podcast_udeb.sis podcast_udeb_signed.sis ..\..\common\certificate\podcast.cer ..\..\common\certificate\podcast.key
