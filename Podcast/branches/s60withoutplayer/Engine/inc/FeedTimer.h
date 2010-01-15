c32fcf7e4d0030b34c50b80613b883443c315995
LarsPson <LarsPson@de3fedf9-773c-0410-aaf4-894f9644ab05>
2010-01-15 11:49:00 +0000

    Add license text to all cpp and h files

--- Podcast/branches/s60withoutplayer/Engine/inc/FeedTimer.h
+++ Podcast/branches/s60withoutplayer/Engine/inc/FeedTimer.h
@@ -1,3 +1,21 @@
+/*
+* Copyright (c) 2007-2010 Sebastian Brannstrom, Lars Persson, EmbedDev AB
+*
+* All rights reserved.
+* This component and the accompanying materials are made available
+* under the terms of the License "Eclipse Public License v1.0"
+* which accompanies this distribution, and is available
+* at the URL "http://www.eclipse.org/legal/epl-v10.html".
+*
+* Initial Contributors:
+* EmbedDev AB - initial contribution.
+*
+* Contributors:
+*
+* Description:
+*
+*/
+
 #ifndef FEEDTIMER_H
 #define FEEDTIMER_H
 
