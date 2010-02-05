caaa75c9698ca0d09cbe6ad6c808ba83bd874e45
anders.fridlund <anders.fridlund@de3fedf9-773c-0410-aaf4-894f9644ab05>
2010-02-05 10:57:44 +0000

    Updated MFeedEngineObserver with more informative names.

--- Podcast/branches/s60withoutplayer/Engine/inc/FeedEngineObserver.h
+++ Podcast/branches/s60withoutplayer/Engine/inc/FeedEngineObserver.h
@@ -20,9 +20,9 @@
 #define FEEDENGINEOBSERVER_H_
 class MFeedEngineObserver {
 public:
-	virtual void FeedInfoUpdated(TUint aFeedUid) = 0;
-	virtual void FeedDownloadUpdatedL(TUint aFeedUid, TInt aPercentOfCurrentDownload) = 0;
-	virtual void FeedUpdateCompleteL(TUint aFeedUid) = 0;
+	virtual void FeedDownloadStartedL(TUint aFeedUid) = 0;
+	virtual void FeedDownloadProgressL(TUint aFeedUid, TInt aPercent) = 0;
+	virtual void FeedDownloadFinishedL(TUint aFeedUid) = 0;
 	virtual void FeedUpdateAllCompleteL() = 0;
 	virtual void FeedSearchResultsUpdated() = 0;
 };
