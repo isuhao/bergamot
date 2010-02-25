ef843f378ee6f41baee90a788e88f7c5eef1c290
teknolog <teknolog@de3fedf9-773c-0410-aaf4-894f9644ab05>
2010-02-25 15:00:36 +0000

    Files moved to Symbian Foundation Mercurial repo at http://developer.symbian.org/oss/FCL/sf/incubator/podcatcher/

--- Podcast/branches/s60withoutplayer/application/sis/make_s60_5th_sis.cmd
+++
@@ -1,6 +0,0 @@
-del *.sis
-makesis -dc:\symbian\S60_5th_Edition_SDK_v1.0 podcatcher_udeb.pkg
-signsis podcatcher_udeb.sis podcatcher_udeb_signed.sis podcast.cer podcast.key
-
-makesis -dc:\symbian\S60_5th_Edition_SDK_v1.0 podcatcher_urel.pkg
-signsis podcatcher_urel.sis podcatcher_urel_signed.sis podcast.cer podcast.key
